8c0e9e1f7748316ea01e111fefb6310d33aa8ed0
febyantihutahaean16 <febyanti.hth.93@gmail.com>
2024-05-07 22:32:17 +0700

    update berita

--- resources/views/News/News.blade.php
+++ resources/views/News/News.blade.php
@@ -2,36 +2,85 @@
 @section("title", "News")
 
 @section('content')
-<section class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                @foreach($news as $berita)
-                <div class="news-card mb-4">
-                    <div class="row align-items-center">
-                        <div class="col-md-4">
-                            <img src="{{ asset('storage/app/public/photo/' . $berita->photo) }}" alt="{{ $berita->title }}" class="img-fluid rounded">
-                        </div>
-                        <div class="col-md-8">
-                            <h5>{{ $berita->title }}</h5>
-                            <!-- Tampilkan hanya sebagian deskripsi -->
-                            <p>{!! $berita->description !!}</p>
-                            <a href="{{ route('news.show', ['id_news' => $berita->id_news]) }}" class="btn btn-primary">See More</a>
-                        </div>
-                    </div>
+
+<div class="container-fluid pb-4 pt-4 paddding">
+    <div class="container paddding">
+        <div class="row mx-0">
+            <div class="col-md-8 animate-box" data-animate-effect="fadeInLeft">
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">News</div>
                 </div>
-                @endforeach
-                <div class="d-flex justify-content-center">
-                    {{ $news->links('pagination::simple-bootstrap-5') }}
-                </div>
-                <ul class="slider-nav">
-                    <!-- You can add slider navigation here if needed -->
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <!-- Sidebar content here -->
-            </div>
+                <div class="row pb-4">
+                    @foreach ($news as $berita)
+    <div class="col-md-5">
+        <div class="fh5co_hover_news_img">
+            <div class="fh5co_news_img"><img src="{{ asset('storage/app/public/photo/' . $berita->photo) }}" alt="{{ $berita->title }}"/></div>
+            <div></div>
         </div>
     </div>
-</section>
+    <div class="col-md-7 animate-box">
+        <a href="{{ route('news.show', ['id_news' => $berita->id_news]) }}" class="fh5co_magna py-2">{{ $berita->title }}</a> <a href="{{ route('news.show', ['id_news' => $berita->id_news]) }}" class="fh5co_mini_time py-3"> {{ $berita->create_by }} - {{ $berita->date }} </a>
+        <div class="fh5co_consectetur">
+            {{-- Batasi deskripsi hanya 4 baris --}}
+            <?php
+                $description = strip_tags($berita->description); // Menghapus tag HTML dari deskripsi
+                $trimmedDescription = Str::limit($description, 200); // Batasi deskripsi hanya 200 karakter
+                $cleanDescription = ltrim($trimmedDescription); // Menghapus spasi di awal teks
+                echo nl2br(e($cleanDescription));
+            ?>
+            @if (strlen($description) > 200)
+                <span class="fh5co_more"><a href="#">...</a></span> {{-- Tambahkan link untuk melihat lebih banyak jika deskripsi dipotong --}}
+            @endif
+        </div>
+    </div>
+@endforeach
+
+
+                </div>
+            </div>
+            <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Kategori</div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="fh5co_tags_all">
+                    <a href="#" class="fh5co_tagg">{{ $berita->category->name }}</a>
+                </div>
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
+                </div>
+                <div class="row pb-3">
+                    <?php
+                        // Acak urutan berita populer
+                        $randomNews = $news->shuffle();
+                    ?>
+                    @foreach ($randomNews as $berita)
+                    <div class="col-5 align-self-center">
+                        <a href="{{ route('news.show', ['id_news' => $berita->id_news]) }}"> <!-- Tautan di sekitar gambar -->
+                            <img src="{{ asset('storage/app/public/photo/' . $berita->photo) }}" alt="{{ $berita->title }}" class="fh5co_most_trading"/>
+                        </a>
+                    </div>
+                    <div class="col-7 paddding">
+                        <a href="{{ route('news.show', ['id_news' => $berita->id_news]) }}"> <!-- Tautan di sekitar judul -->
+                            <div class="most_fh5co_treding_font">{{ $berita->title }}</div>
+                        </a>
+                        <div class="most_fh5co_treding_font_123">{{ $berita->created_at->format('F d, Y') }}</div>
+                    </div>
+                @endforeach
+                </div>
+                
+            </div>
+        </div>
+        <div class="row mx-0 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-12 text-center pb-4 pt-4">
+                <a href="#" class="btn_mange_pagging"><i class="fa fa-long-arrow-left"></i>&nbsp;&nbsp; Previous</a>
+                <a href="#" class="btn_pagging">1</a>
+                <a href="#" class="btn_pagging">2</a>
+                <a href="#" class="btn_pagging">3</a>
+                <a href="#" class="btn_pagging">...</a>
+                <a href="#" class="btn_mange_pagging">Next <i class="fa fa-long-arrow-right"></i>&nbsp;&nbsp; </a>
+             </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/News/show.blade.php
+++ resources/views/News/show.blade.php
@@ -1,21 +1,196 @@
 @extends("layouts.layout")
 @section("title", "News Detail")
-
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+@endsection
 @section('content')
-<section class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4 text-center">
-                <div class="news-detail">
-                    <img src="{{ asset('storage/app/public/photo/' . $news->photo) }}" alt="{{ $news->title }}" class="img-fluid ">
-                </div>
+
+<!-- Modal Search Start -->
+<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content rounded-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Search by keyword</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="text-center mt-4">
-                <h2>{{ $news->title }}</h2>
-                <p class="text-break">{!! $news->description !!}</p>
-                <p>{{ $news->date }}</p>
+            <div class="modal-body d-flex align-items-center">
+                <div class="input-group w-75 mx-auto d-flex">
+                    <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                    <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+                </div>
             </div>
         </div>
     </div>
-</section>
+</div>
+<!-- Modal Search End -->
+
+
+<!-- Single Product Start -->
+<div class="container-fluid py-12">
+    <div class="container py-5">
+        <ol class="breadcrumb justify-content-start mb-4">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">News</a></li>
+            <li class="breadcrumb-item active text-dark">{{ $news->date}}</li>
+        </ol>
+        <div class="row g-4">
+            <div class="col-lg-8">
+                <div class="mb-4">
+                    <a href="#" class="h1 display-5">{{ $news->title }}</a>
+                </div>
+                <div class="position-relative rounded overflow-hidden mb-3">
+                    <img src="{{ asset('storage/app/public/photo/' . $news->photo) }}" class="img-zoomin img-fluid rounded w-100" alt="">
+                    <div class="position-absolute text-white px-4 py-2 bg-primary rounded" style="top: 20px; right: 20px;">                                              
+                        {{!! $news->category->name!!}}
+                    </div>
+                </div>
+                {{-- <div class="d-flex justify-content-between">
+                    <a href="#" class="text-dark link-hover me-3"><i class="fa fa-clock"></i> 06 minute read</a>
+                    <a href="#" class="text-dark link-hover me-3"><i class="fa fa-eye"></i> 3.5k Views</a>
+                    <a href="#" class="text-dark link-hover me-3"><i class="fa fa-comment-dots"></i> 05 Comment</a>
+                    <a href="#" class="text-dark link-hover"><i class="fa fa-arrow-up"></i> 1.5k Share</a>
+                </div> --}}
+                <p class="my-4">{!! $news->description !!}</p>
+                
+                <div class="tab-class">
+                    <div class="d-flex justify-content-between border-bottom mb-4">
+                        <ul class="nav nav-pills d-inline-flex text-center">
+                            <li class="nav-item mb-3">
+                                <h5 class="mt-2 me-3 mb-0">Tags:</h5>
+                            </li>
+                            <li class="nav-item mb-3">
+                                <a class="d-flex py-2 bg-light rounded-pill active me-2" data-bs-toggle="pill" href="#tab-1">
+                                    <span class="text-dark" style="width: 100px;">{{ $news->category->name}}</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="d-flex align-items-center">
+                            <h5 class="mb-0 me-3">Share:</h5>
+                            <i class="fab fa-facebook-f link-hover btn btn-square rounded-circle border-primary text-dark me-2"></i>
+                            <i class="btn fab bi-twitter link-hover btn btn-square rounded-circle border-primary text-dark me-2"></i>
+                            <i class="btn fab fa-instagram link-hover btn btn-square rounded-circle border-primary text-dark me-2"></i>
+                            <i class="btn fab fa-linkedin-in link-hover btn btn-square rounded-circle border-primary text-dark"></i>
+                        </div>
+                    </div>
+                <div class="bg-light rounded my-4 p-4">
+                    <h4 class="mb-4">You Might Also Like</h4>
+                    <div class="row g-4">
+                        <div class="col-lg-6">
+                            <div class="d-flex align-items-center p-3 bg-white rounded">
+                                <img src="img/chatGPT.jpg" class="img-fluid rounded" alt="">
+                                <div class="ms-3">
+                                    <a href="#" class="h5 mb-2">Lorem Ipsum is simply dummy text of the printing</a>
+                                    <p class="text-dark mt-3 mb-0 me-3"><i class="fa fa-clock"></i> 06 minute read</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="d-flex align-items-center p-3 bg-white rounded">
+                                <img src="img/chatGPT-1.jpg" class="img-fluid rounded" alt="">
+                                <div class="ms-3">
+                                    <a href="#" class="h5 mb-2">Lorem Ipsum is simply dummy text of the printing</a>
+                                    <p class="text-dark mt-3 mb-0 me-3"><i class="fa fa-clock"></i> 06 minute read</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-light rounded p-4">
+                    <h4 class="mb-4">Comments</h4>
+                    <div class="p-4 bg-white rounded mb-4">
+                        <div class="row g-4">
+                            <div class="col-3">
+                                <img src="img/footer-4.jpg" class="img-fluid rounded-circle w-100" alt="">
+                            </div>
+                            <div class="col-9">
+                                <div class="d-flex justify-content-between">
+                                    <h5>James Boreego</h5>
+                                    <a href="#" class="link-hover text-body fs-6"><i class="fas fa-long-arrow-alt-right me-1"></i> Reply</a>
+                                </div>
+                                <small class="text-body d-block mb-3"><i class="fas fa-calendar-alt me-1"></i> Dec 9, 2024</small>
+                                <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy Lorem Ipsum has been the industry's standard dummy type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4 bg-white rounded mb-0">
+                        <div class="row g-4">
+                            <div class="col-3">
+                                <img src="img/footer-4.jpg" class="img-fluid rounded-circle w-100" alt="">
+                            </div>
+                            <div class="col-9">
+                                <div class="d-flex justify-content-between">
+                                    <h5>James Boreego</h5>
+                                    <a href="#" class="link-hover text-body fs-6"><i class="fas fa-long-arrow-alt-right me-1"></i> Reply</a>
+                                </div>
+                                <small class="text-body d-block mb-3"><i class="fas fa-calendar-alt me-1"></i> Dec 9, 2024</small>
+                                <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy Lorem Ipsum has been the industry's standard dummy type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-light rounded p-4 my-4">
+                    <h4 class="mb-4">Leave A Comment</h4>
+                    <form action="#">
+                        <div class="row g-4">
+                            <div class="col-lg-6">
+                                <input type="text" class="form-control py-3" placeholder="Full Name">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="email" class="form-control py-3" placeholder="Email Address">
+                            </div>
+                            <div class="col-12">
+                                <textarea class="form-control" name="textarea" id="" cols="30" rows="7" placeholder="Write Your Comment Here"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button class="form-control btn btn-primary py-3" type="button">Submit Now</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <!-- Sidebar Start -->
+                <div class="row g-4">
+                    <div class="col-12">
+                        <!-- Search Form -->
+                        <div class="p-3 rounded border bg-light mb-4">
+                            <h4 class="mb-3">Search</h4>
+                            <form action="#">
+                                <div class="input-group">
+                                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search" aria-describedby="button-addon2">
+                                    <button class="btn btn-outline-primary" type="button" id="button-addon2">Search</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- Popular Categories -->
+                        <div class="p-3 rounded border bg-light mb-4">
+                            <h4 class="mb-3">Popular Categories</h4>
+                            <ul class="list-unstyled">
+                                {{-- @foreach($popularCategories as $category)
+                                <li><a href="#">{{ $category->name }}</a></li> --}}
+                                {{-- @endforeach --}}
+                            </ul>
+                        </div>
+                        <!-- Popular News -->
+                        <div class="p-3 rounded border bg-light mb-4">
+                            <h4 class="mb-3">Popular News</h4>
+                            <div class="list-group">
+                                {{-- @foreach($popularNews as $newsItem)
+                                <a href="#" class="list-group-item list-group-item-action">{{ $newsItem->title }}</a>
+                                @endforeach --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Sidebar End -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Single Product End -->
+
+
 @endsection
+
