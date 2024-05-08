072f49503fc8564868e90cb0d18c365ceeb86286
roysigalingging <165919786+roysigalingging@users.noreply.github.com>
2024-05-08 09:27:48 +0700

    next

--- resources/views/News/News.blade.php
+++ resources/views/News/News.blade.php
@@ -79,15 +79,9 @@
                 </div>
             </div>
         </div>
-        <div class="row mx-0 animate-box" data-animate-effect="fadeInUp">
-            <div class="col-12 text-center pb-4 pt-4">
-                <a href="#" class="btn_mange_pagging"><i class="fa fa-long-arrow-left"></i>&nbsp;&nbsp; Previous</a>
-                <a href="#" class="btn_pagging">1</a>
-                <a href="#" class="btn_pagging">2</a>
-                <a href="#" class="btn_pagging">3</a>
-                <a href="#" class="btn_pagging">...</a>
-                <a href="#" class="btn_mange_pagging">Next <i class="fa fa-long-arrow-right"></i>&nbsp;&nbsp; </a>
-             </div>
+        <div class="d-flex justify-content-end mt-3">
+            {{ $news->links('pagination::simple-bootstrap-5') }}
+        </div>
         </div>
     </div>
 </div>
